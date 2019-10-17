2505baa44922db3a399a7f9053889856df998adc
Noah Sissons <constro49@gmail.com>
2019-10-17 15:41:27 -0400

    added an early sql file to create the tables required for the first increment.

---
+++ masterscript.sql
@@ -0,0 +1,45 @@
+connect to ecommerce;
+
+create table User(
+    emailAddress varchar(255),
+    passwordHash varchar(255),
+    administrator bit,
+    primary key (emailAddress)
+);
+
+create table Seller(
+    userEmail varchar(255),
+    sellerName varchar(255),
+    primary key (sellerName),
+    foreign key (userEmail) references User (emailAddress)
+);
+
+create table Product(
+    productID integer,
+    price double,
+    stock integer,
+    description varchar(511),
+    manufacturer char,
+    primary key (productID)
+);
+
+create table Listing(
+    sellerName varchar(255),
+    productID integer,
+    primary key (sellerName, productID),
+    foreign key (sellerName) references Seller (sellerName),
+    foreign key (productID) references Product (productID)
+);
+
+create table Order(
+    orderID integer,
+    buyerEmail varchar(255),
+    sellerName varchar(255),
+    productID integer,
+    totalPrice double,
+    sourceAddress varchar(255),
+    custAddress varchar(255),
+    primary key (orderID),
+    foreign key (buyerEmail) references User (emailAddress),
+    foreign key (sellerName, productID) references Listing (sellerName, productID)
+);
